78b811162dff78c2bbb7bd67f660863e9d7ebca4
MarcosLemos-dev <dasilvalemosm@gmail.com>
2023-04-07 22:12:38 -0300

    Subindo o projeto de dicionarios de arquivos com a função de criar arquivo txt com lista de dicionario e a funcionalidade de listar todos os dicionario presente no arquivo txt , e por fim arquivos .py manipulando arquivo de texto

--- dicionario_arquivo/ManageUsers.py
+++ dicionario_arquivo/ManageUsers.py
@@ -3,10 +3,12 @@ usuarios={}
 opcao=perguntar()
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L" :
     if opcao=="I":
+
         inserir(usuarios)
         opcao = perguntar()
+
     if opcao == "L":
         listar(usuarios)
         opcao = perguntar()
 else:
-    print("script parou")
+    print("script parou, clique em executar ativar o script")
