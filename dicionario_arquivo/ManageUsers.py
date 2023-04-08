590da6f4b64f03188e99699d484b58fc8acdd8c3
MarcosLemos-dev <dasilvalemosm@gmail.com>
2023-04-07 23:34:32 -0300

    adicionando comentario

--- dicionario_arquivo/ManageUsers.py
+++ dicionario_arquivo/ManageUsers.py
@@ -1,11 +1,13 @@
 from dicionario_arquivo.Funcoes import *
 usuarios={}
 opcao=perguntar()
+
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L" :
+    #inserir novos usuarios para um novo dicionario de dados
     if opcao=="I":
         inserir(usuarios)
         opcao = perguntar()
-
+    # listar todos os usuarios dicionario de dados
     if opcao == "L":
         listar(usuarios)
         opcao = perguntar()
